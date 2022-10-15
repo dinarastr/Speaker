c7b66f74bfe665cd623566890b910a11794448f5
Dinara.Stepina <dinara.stepina@waveaccess.ru>
2022-10-15 16:21:32 +0300

    added new fragments

--- app/src/main/java/ru/dinarastepina/speaker/SplashActivity.kt
+++ app/src/main/java/ru/dinarastepina/speaker/SplashActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var handler:Handler
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        handler = Handler(Looper.getMainLooper())
         supportActionBar?.hide()
 
-        Handler().postDelayed(
-            { startActivity(Intent(this, MainActivity::class.java))
+        handler.postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
         },
             2000)
     }
