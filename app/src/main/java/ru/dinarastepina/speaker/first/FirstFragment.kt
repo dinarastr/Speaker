5fed011e53cd473dd71902756d7944112966699d
Dinara.Stepina <dinara.stepina@waveaccess.ru>
2022-10-21 19:21:16 +0300

    added nav to the 2nd fragment

--- app/src/main/java/ru/dinarastepina/speaker/first/FirstFragment.kt
+++ app/src/main/java/ru/dinarastepina/speaker/first/FirstFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import ru.dinarastepina.speaker.R
+import ru.dinarastepina.speaker.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private lateinit var vb: FragmentFirstBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        vb = FragmentFirstBinding.inflate(layoutInflater, container, false)
+
+        vb.face.setOnClickListener {
+            val action = FirstFragmentDirections.firstToSecond("семья")
+            vb.root.findNavController().navigate(action)
+        }
+
+        return vb.root
     }
 }
--- app/src/main/java/ru/dinarastepina/speaker/second/SecondFragment.kt
+++ app/src/main/java/ru/dinarastepina/speaker/second/SecondFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import ru.dinarastepina.speaker.R
+import ru.dinarastepina.speaker.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private lateinit var vb: FragmentSecondBinding
+    private val args by navArgs<SecondFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        vb = FragmentSecondBinding.inflate(layoutInflater, container, false)
+
+        vb.secondTv.text = "Тема: ${args.info}"
+        return vb.root
     }
 }
